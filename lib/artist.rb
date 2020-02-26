42b2be40e4fa587ab985c36a3e771c071dbb7ac0
Cathrin Whitmer <catwhitmer@gmail.com>
2020-02-26 01:25:46 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -26,9 +26,7 @@ class Artist
     self.class.all << self
   end
   
-  def self.find_or_create_by_name(name)
-    self.all.detect {|artist| artist.name == name} || Artist.new(name).save
-  end
+
   
   def print_songs
     @songs.each do |song|
