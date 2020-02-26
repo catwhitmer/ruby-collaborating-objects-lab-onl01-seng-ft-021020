70162e28e43aa16894b8c3d66afad0d9e1320e97
Cathrin Whitmer <catwhitmer@gmail.com>
2020-02-26 00:47:20 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,40 @@
+class Artist 
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def add_song(song)
+    @songs << song 
+    song.artist = self 
+    song
+  end
+  
+  def songs 
+    @songs
+  end
+  
+  def save
+    @@all << self
+    self
+  end
+  
+  def self.find_or_create_by_name(name)
+    self.all.detect {|artist| artist.name == name} || Artist.new(name).save
+  end
+  
+  def print_songs
+    @songs.each do |song|
+    puts song.name
+  end
+ end
+  
+end
